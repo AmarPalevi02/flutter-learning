e55687837fcf4a63eaf91ba125ece3a2ea76ec22
amarNrddn <amarnrddn02@gmail.com>
2023-12-22 00:24:13 +0700

    update form input

--- lib/FormInput.dart
+++ lib/FormInput.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:uas/api/getData.dart';
 
 class FormInput extends StatefulWidget {
   const FormInput({super.key});
@@ -8,82 +11,174 @@ class FormInput extends StatefulWidget {
 }
 
 class _FormInputState extends State<FormInput> {
-  String? _value;
+  String? _goodOrBad;
+  String? _kode = '';
+  String? _judul = '';
   String? _prodi;
+  DateTime? _tglBeli;
+  String? _jenis;
+  File? _selectedImage;
+
+  Future _pickImageFromGallerry() async {
+    final returnImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      _selectedImage = File(returnImage!.path);
+    });
+  }
+
+  TextEditingController kodeBukuController = TextEditingController();
+  TextEditingController JudulBukuController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.only(top: 60, left: 15, right: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Tambah Categories Buku',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 60, left: 15, right: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Tambah Categories Buku',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Kode', hintText: 'Masukan Kode Buku'),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  'Kondisi',
+                TextField(
+                  controller: kodeBukuController,
+                  decoration: InputDecoration(
+                    labelText: 'Kode',
+                    hintText: 'Masukan Kode Buku',
+                  ),
                 ),
-              ),
-              Row(
-                children: <Widget>[
-                  Radio(
-                      value: 'Baik',
-                      groupValue: _value,
-                      onChanged: (value) {
+                TextField(
+                  controller: JudulBukuController,
+                  decoration: InputDecoration(
+                    labelText: 'Judul',
+                    hintText: 'Masukan Judul Buku',
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: Text(
+                    'Kondisi',
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Radio(
+                        value: 'Baik',
+                        groupValue: _goodOrBad,
+                        onChanged: (value) {
+                          setState(() {
+                            _goodOrBad = value!;
+                            print(_goodOrBad);
+                          });
+                        }),
+                    Text('Baik'),
+                    Radio(
+                        value: 'Rusak',
+                        groupValue: _goodOrBad,
+                        onChanged: (value) {
+                          setState(() {
+                            _goodOrBad = value!;
+                            print(_goodOrBad);
+                          });
+                        }),
+                    Text('Rusak'),
+                  ],
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      showDatePicker(
+                              context: context,
+                              initialDate: _tglBeli ?? DateTime.now(),
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime.now())
+                          .then((date) {
                         setState(() {
-                          _value = value!;
-                          print(_value);
+                          _tglBeli = date!;
+                          print(_tglBeli);
                         });
-                      }),
-                  Text('Baik'),
-                  Radio(
-                      value: 'Rusak',
-                      groupValue: _value,
-                      onChanged: (value) {
-                        setState(() {
-                          _value = value!;
-                          print(_value);
-                        });
-                      }),
-                  Text('Rusak'),
-                ],
-              ),
-              DropdownButtonFormField<String>(
-                value: _prodi,
-                onChanged: (value) {
-                  setState(() {
-                    _prodi = value!;
-                    print(_prodi);
-                  });
-                },
-                items: <String>[
-                  'Teknik Mesin',
-                  'Teknik Industri',
-                  'Teknik Informatika',
-                  'Teknik Sipil'
-                ].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem(value: value, child: Text(value));
-                }).toList(),
-                decoration: InputDecoration(labelText: 'Prodi'),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Judul', hintText: 'Masukan Judul Buku'),
-              ),
-            ],
+                      });
+                    },
+                    child: Text('Masukan Tanggal Beli')),
+                DropdownButtonFormField<String>(
+                  value: _prodi,
+                  onChanged: (value) {
+                    setState(() {
+                      _prodi = value!;
+                      print(_prodi);
+                    });
+                  },
+                  items: <String>[
+                    'Teknik Mesin',
+                    'Teknik Industri',
+                    'Teknik Informatika',
+                    'Teknik Sipil'
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem(value: value, child: Text(value));
+                  }).toList(),
+                  decoration: InputDecoration(labelText: 'Prodi'),
+                ),
+                DropdownButtonFormField<String>(
+                  value: _jenis,
+                  onChanged: (value) {
+                    setState(() {
+                      _jenis = value!;
+                      print(_jenis);
+                    });
+                  },
+                  items: <String>[
+                    'Fiksi',
+                    'Non-Fiksi',
+                    'Ensiklopedia',
+                    'Referensi',
+                    'Pengembangan'
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem(value: value, child: Text(value));
+                  }).toList(),
+                  decoration: InputDecoration(labelText: 'Jenis Buku'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _pickImageFromGallerry();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Pilih Dari Galery',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                // _selectedImage != null
+                //     ? Image.file(_selectedImage!)
+                //     : Text('Pilih image'),
+                ElevatedButton(
+                    onPressed: () {
+                      fetchDatas().addBook(
+                          goodOrBad: _goodOrBad,
+                          kode: kodeBukuController.text,
+                          judul: JudulBukuController.text,
+                          prodi: _prodi,
+                          tglBeli: _tglBeli.toString(),
+                          jenis: _jenis,
+                          selectedImage: _selectedImage);
+                    },
+                    child: Text('Tambah'))
+              ],
+            ),
           ),
         ),
       ),
--- lib/api/getData.dart
+++ lib/api/getData.dart
@@ -2,8 +2,8 @@ import 'package:dio/dio.dart';
 
 class fetchDatas {
   Future<List<Map<String, dynamic>>> getDatas() async {
-    final result =
-        await Dio().get('http://192.168.111.38:9000/api/v1/cms/draftCategories');
+    final result = await Dio()
+        .get('http://192.168.111.38:9000/api/v1/cms/draftCategories');
 
     if (result.statusCode == 200) {
       final datas = result.data['data'];
@@ -15,5 +15,33 @@ class fetchDatas {
     }
   }
 
- 
+  Future addBook({
+    required final goodOrBad,
+    required final kode,
+    required final judul,
+    required final prodi,
+    required final tglBeli,
+    required final jenis,
+    required final selectedImage,
+  }) async {
+    try {
+      String fileName = selectedImage.path.split('/').last;
+      FormData formData = FormData.fromMap({
+        'kBuku': kode,
+        'judul': judul,
+        'tglBeli': tglBeli,
+        'kondisi': goodOrBad,
+        'prodi': prodi,
+        'jenis': jenis,
+        'fileName': await MultipartFile.fromFile(selectedImage.path,
+            filename: fileName),
+      });
+
+      await Dio().post('http://192.168.76.38/:9000/api/v1/cms/draftCategories',
+          data: formData);
+    } catch (e) {
+      print(e);
+      return e;
+    }
+  }
 }
